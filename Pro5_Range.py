54fa898d8a2c334dfec911a868bbe4300240d94b
Gautam Yadav <93143267+unique-gautam-yadav@users.noreply.github.com>
2022-08-22 13:08:08 -0700

    Program 5 Done

---
+++ Pro5_Range.py
@@ -0,0 +1,3 @@
+num = int(input("Enter A Number -->"))
+for n in range (1, 11, 1):
+    print(num * n)
---
+++ Test/while.py
@@ -0,0 +1,9 @@
+from pickle import FALSE
+
+
+a = True
+
+while (a == False):
+    print ("Hello World")
+else:
+    print ("World Hellow")
