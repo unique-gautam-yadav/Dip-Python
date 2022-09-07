a09ace43117381f5b502320113fa930e978fd028
Gautam Yadav <gautamyadav08112005@gmail.com>
2022-09-07 12:56:05 +0530

    Program 10 Done

---
+++ Pro10_List.py
@@ -0,0 +1,27 @@
+
+a = ['Gautam', 'Hello', 'asdklj', 34, 21,  42.2]
+inn = 2
+s = 'ge'
+f = 3.3
+
+
+inn = type(inn)
+s = type(s)
+f = type(f)
+
+
+strings = 0
+intger = 0
+floats = 0
+
+
+for i in a:
+    if (type(i) == inn):
+        intger += 1
+    elif (type(i) == s):
+        strings += 1
+    elif (type(i) == f):
+        floats += 1
+
+
+print('Strings =', strings, 'integers =', intger, 'floats =', floats)
---
+++ Test/draw.py
@@ -0,0 +1,10 @@
+from turtle import *
+import turtle
+
+tur = turtle.Turtle()
+
+tur.fillcolor("cyan")
+
+tur.circle(100)
+
+turtle.done()
